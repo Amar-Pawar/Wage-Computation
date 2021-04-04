9f6828508a0ba52a8c926213f4cd20e16850d892
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-04-04 19:40:16 +0530

    add:part time employee and wages added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -x
 isAbsent=0
+fullTimePresent=1
 wagePerHr=20
 fullDayHr=8
-empCheck=$((RANDOM%2))
+partTimeHr=8
+empCheck=$((RANDOM%3))
 if [ $empCheck -eq 0 ]
 then
 	echo "Absent"
+elif [ $empCheck -eq 1 ]
+then
+	echo "employee full time present"
+	empDailyWages=$(($wagePerHr*$fullDayHr))
 else
-	echo "present"
+	echo "employee is part time present"
+	empDailyWages=$(($wagePerHr*$partTimeHr))
 fi
-empDailyWages=$(($wagePerHr*$fullDayHr))
