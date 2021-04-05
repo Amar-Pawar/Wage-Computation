1b19e00a68b72eac3e0675edc55f74dbc8917f4a
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-04-05 12:07:05 +0530

    add:wages till condition of hours or days reached

---
+++ wagetill.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+day=0
+hr=0
+dailyhr=10
+workday=1
+perHrWage=20
+while [ $hr != 100 -o $day != 20 ]
+do
+empCheck=$((RANDOM%3))
+if [ $empCheck -eq 1 ]
+then
+	echo "full time present"
+	hr=$(($hr+$dailyhr))
+	day=$(($day+$workday))
+	if [ $hr -eq 100 ]
+	then
+		break
+	elif [ $day -eq 20 ]
+	then
+		break
+	fi
+
+elif [ $empCheck -eq 2 ]
+then
+        echo "part time present"
+        hr=$(($hr+$dailyhr))
+        day=$(($day+$workday))
+        if [ $hr -eq 100 ]
+        then
+                break
+	elif [ $day -eq 20 ]
+	then 
+		break
+        fi
+
+else
+	echo "absent"
+        day=$(($day+$workday))
+        if [ $hr -eq 100 ]
+        then
+                break
+	elif [ $day -eq 20 ]
+	then
+		break
+        fi
+fi
+done
+wages=$(($perHrWage*$hr))
