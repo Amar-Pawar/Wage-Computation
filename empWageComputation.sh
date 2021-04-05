1927fce71e9c624ae2d5748b8c544604e764e3ff
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-04-05 09:37:20 +0530

    add:monthly wages added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,16 +2,17 @@
 partTimePresent=2
 fullTimePresent=1
 wagePerHr=20
+workingDaysPerMonth=20
 empCheck=$((RANDOM%3))
 
 case $empCheck in
 	$fullTimePresent)
 		fullDayHr=8
-		empDailyWages=$(($fullDayHr*$wagePerHr))
+		empDailyWages=$(($fullDayHr*$wagePerHr*$workingDaysPerMonth))
 		;;
 	$partTimePresent)
 		partTimeHr=8
-		empDailyWages=$(($partTimeHr*$wagePerHr))
+		empDailyWages=$(($partTimeHr*$wagePerHr*$workingDaysPerMonth))
 		;;
 	*)
 		echo "absent: no salary"
